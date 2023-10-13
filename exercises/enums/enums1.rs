dcf37cb3bff8fd48501088293cc89968f16c62bf
a hardworking ant <84758951+ahardworkingant@users.noreply.github.com>
2023-10-13 09:34:53 +0000

    save data

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,16 +2,17 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    ChangeColor,
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
     println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
+    println!("{:?}", Message::Echo);
     println!("{:?}", Message::ChangeColor);
 }
